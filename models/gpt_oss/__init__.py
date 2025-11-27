2423241c6f87bed95061635f3a15bff15a94f1c7
aryamazaheri <aryanet@gmail.com>
2025-11-27 18:04:31 +0000

    fix gpt_oss bug

--- models/gpt_oss/__init__.py
+++ models/gpt_oss/__init__.py
@@ -7,28 +7,28 @@
 from torchtitan.components.ft.diloco import FaultTolerantTrainSpec, fragment_llm
 from torchtitan.components.loss import build_cross_entropy_loss
 from torchtitan.components.lr_scheduler import build_lr_schedulers
-from torchtitan.components.optimizer import build_optimizers
+from torchtitan.components.optimizer import build_optimizers_with_moe_load_balancing
 from torchtitan.components.tokenizer import build_hf_tokenizer
-from torchtitan.components.validate import build_validator
-from torchtitan.distributed.pipeline_parallel import pipeline_llm
-from torchtitan.hf_datasets.text_datasets import build_text_dataloader
+from torchtitan.datasets.hf_datasets import build_hf_dataloader
 from torchtitan.protocols.train_spec import TrainSpec
 
-from torchtitan.models.llama3 import llama3_args, Llama3StateDictAdapter, parallelize_llama, Transformer
+from torchtitan.experiments.gpt_oss.infra.parallelize import parallelize_gptoss
+from torchtitan.experiments.gpt_oss.model.args import GptOssModelArgs
+from torchtitan.experiments.gpt_oss.model.model import GptOssModel
+
+from torchtitan.experiments.gpt_oss import gptoss_configs
 
 
 def get_train_spec() -> TrainSpec:
     return FaultTolerantTrainSpec(
-        model_cls=Transformer,
-        model_args=llama3_args,
-        parallelize_fn=parallelize_llama,
-        pipelining_fn=pipeline_llm,
-        build_optimizers_fn=build_optimizers,
+        model_cls=GptOssModel,
+        model_args=gptoss_configs,
+        parallelize_fn=parallelize_gptoss,
+        pipelining_fn=None,
+        build_optimizers_fn=build_optimizers_with_moe_load_balancing,
         build_lr_schedulers_fn=build_lr_schedulers,
-        build_dataloader_fn=build_text_dataloader,
+        build_dataloader_fn=build_hf_dataloader,
         build_tokenizer_fn=build_hf_tokenizer,
         build_loss_fn=build_cross_entropy_loss,
-        build_validator_fn=build_validator,
-        state_dict_adapter=Llama3StateDictAdapter,
         fragment_fn=fragment_llm,
     )
